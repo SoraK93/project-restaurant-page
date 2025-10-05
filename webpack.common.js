62170ae459d0cc005a043ea6f3f68574ba318d26
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2025-10-05 15:59:46 +0530

    Fix: webpack & package.json ready for both development and production

--- webpack.common.js
+++ webpack.common.js
@@ -1,14 +1,8 @@
 const path = require("path");
-const HTMLWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
   entry: {
     index: "./src/js/index.js",
-  },
-  devtool: "eval-source-map",
-  devServer: {
-    watchFiles: ["./src/*.html", "./src/css/*.css"],
   },
   plugins: [
     new HTMLWebpackPlugin({
@@ -22,19 +16,14 @@ module.exports = {
         loader: "html-loader",
       },
       {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
         test: /\.(jpg|jpeg|webp)$/i,
         type: "asset/resource",
       },
     ],
   },
   output: {
-    filename: "main.js",
     path: path.resolve(__dirname, "dist"),
-    publicPath: '/project-restaurant-page.git/',
+    publicPath: '/project-restaurant-page/',
     clean: true,
   },
 };
---
+++ webpack.dev.js
@@ -0,0 +1,26 @@
+const merge = require("webpack-merge");
+const common = require("webpack.common.js");
+
+module.exports = merge(common, {
+  mode: "development",
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/*.html", "./src/css/*.css"],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  output: {
+    filename: "main.js",
+  },
+});
